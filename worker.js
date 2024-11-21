9b5477fc78015f03c8582d51176e2d42c92ea187
yindin777 <yindin777@gmail.com>
2024-11-21 09:25:36 +0100

    Delete worker.js

--- worker.js
+++
@@ -1,5 +0,0 @@
-self.onmessage = function(event) {
-    // Handle messages and perform background tasks
-    console.log('Background task started:', event.data);
-    self.postMessage('Background task completed');
-};
